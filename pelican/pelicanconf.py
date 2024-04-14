3de4a9218e489122330670e4a30a579bab3e5ec2
Mikael Honkala <mikael.honkala@gmail.com>
2024-04-14 22:05:56 +0300

    Theme change

--- pelican/pelicanconf.py
+++ pelican/pelicanconf.py
@@ -10,28 +10,19 @@ TIMEZONE = 'Europe/Helsinki'
 
 DEFAULT_LANG = 'en'
 
+THEME = "simple"
+
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
-
+TAGS_SAVE_AS = ''
+TAG_SAVE_AS = ''
 # Blogroll
 LINKS = (
-    ("Pelican", "https://getpelican.com/"),
-    ("Python.org", "https://www.python.org/"),
-    ("Jinja2", "https://palletsprojects.com/p/jinja/"),
-    ("You can modify those links in your config file", "#"),
-)
-
-# Social widget
-SOCIAL = (
-    ("You can add links in your config file", "#"),
-    ("Another social link", "#"),
+    ("Github", "https://github.com/mikaelho/dynascope"),
 )
 
 DEFAULT_PAGINATION = False
-
-# Uncomment following line if you want document-relative URLs when developing
-# RELATIVE_URLS = True
